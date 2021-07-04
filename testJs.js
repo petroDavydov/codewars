530eac1a6dde34310b4aa7cc698325c7b6482cb8
petroDavydov <petrodavydov@gmail.com>
2021-07-04 14:47:48 +0300

    new file test

---
+++ testJs.js
@@ -0,0 +1,11 @@
+// console.log("hello");
+
+// console.log(('xo')); //true
+// console.log(("xxOo")); //true
+// console.log(("xxxm"));// false
+// console.log(("Oo"));// false
+// console.log(("ooom"));// false
+
+// console.log('Hello');
+
+// window.console.log('test')
